9bfab7b720fe0beb5dcfe15a17879bc371c77113
htrujillo <hector.trujillo.dotnet@gmail.com>
2024-08-05 03:46:55 -0600

    [HITG-02] -New: Adding SearchController

---
+++ BuscandoMiTrago/Controllers/SearchController.cs
@@ -0,0 +1,35 @@
+﻿using BuscandoMiTrago.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BuscandoMiTrago.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public SearchController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
