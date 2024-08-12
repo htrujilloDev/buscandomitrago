4a743b3be8dd8e82e79df32d32137c69d07749d8
htrujillo <hector.trujillo.dotnet@gmail.com>
2024-08-12 01:40:54 -0600

    [HITG-11]-new: Change Look and Fill

--- BuscandoMiTrago/Models/ViewModels/VMBuscandoIngrediente.cs
+++ BuscandoMiTrago/Models/ViewModels/VMBuscandoIngrediente.cs
@@ -1,12 +1,20 @@
-﻿namespace BuscandoMiTrago.Models.ViewModels
+﻿using System.Text.Json.Serialization;
+
+namespace BuscandoMiTrago.Models.ViewModels
 {
     public class VMBuscandoIngrediente
     {
+        [JsonPropertyName("Id")]
         public string? idIngredient { get; set; }
+        [JsonPropertyName("Ingrediente")]
         public string? strIngredient { get; set; }
+        [JsonPropertyName("Descripción")]
         public string? strDescription { get; set; }
+        [JsonPropertyName("Tipo")]
         public string? strType { get; set; }
+        [JsonPropertyName("Alcohol")]
         public string? strAlcohol { get; set; }
+        [JsonPropertyName("ABV")]
         public string? strABV { get; set; }
     }
 }
